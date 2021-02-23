e2a4bfc1e9a90cd2e00d349dd161b8074f2406af
a <b>
2021-02-23 21:28:43 +0800

    feat: 享元模式

--- Design_Pattern/结构型模式/享元 Flyweight.playground/Contents.swift
+++ Design_Pattern/结构型模式/享元 Flyweight.playground/Contents.swift
@@ -31,13 +31,36 @@ import Cocoa
  
 
 ## 适用性
-
+* 一个应用使用了大量对象
+* 完全由于使用了大量对象，产生了很大的存储开销
+* 对象的大多数状态都可以变成外部状态
+* 如果删除对象的外部状态，那么可以用相对少的共享对象取掉很多组对象
+* 应用不依赖于对象标识，由于Flyweight对象可以被共享，对于概念上明显有别的对象，标识测试将返回真值
+ 
 ## 结构
 ![](5.png)
 
 ## 参与者
+ * Flyweight（Glyph）
+  -- 描述一个接口，通过这个接口可以接受并作用于外部状态
+ 
+ * ConcreteFlyweight （Character）
+ -- 实现Flyweight接口，并为内部状态（如果有的话）增加存储空间。ConcreteFlyweight对象必须是可共享的，他所存储的状态必须是内部的；它必须独立于ConcreteFlyweight对象的场景
+ 
+ * UnsharedConcreteFlyweight （Row Column）
+ -- 并非所有的Flyweight子类都需要被共享。Flyweight接口使共享成为可能，但它不强制共享。在Flyweight对象结构的某些层次，UnsharedConcreteFlyweight对象通常将ConcreteFlyweight对象作为子节点（Row和Column就是这样）
+ 
+ * FlyweightFactory
+ -- 创建并管理flyweight对象
+ -- 确保合理的共享Flyweight，当用户请求一个Flyweight时，FlyweightFactory对象提供一个已创建的实例或者创建一个。
+ 
+ * Client
+ -- 维持一个对Flyweight的引用
+ -- 计算或者存储一个（多个）Flyweight的外部状态。
 
 ## 协作
+ * Flyweight执行所需的状态必定是内部或外部的。内部状态存储于ConcreteFlyweight对象中，外部状态则由client存储或者计算。当对象调用Flyweight对象的操作时，将外部状态传递给它
+ * 用户不应该直接对ConcreteFlyweight初始化，而由FlyweightFactory获取
 
 
 ## 效果
